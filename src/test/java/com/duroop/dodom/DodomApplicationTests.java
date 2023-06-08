68551072a585831e83debd9d6a90a33f25cb6ad6
kdmstj <rkddmstj429@gmail.com>
2023-06-08 18:40:23 +0900

    [init] fist init

---
+++ src/test/java/com/duroop/dodom/DodomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duroop.dodom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
