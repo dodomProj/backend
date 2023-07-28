0a1f9da82a8856f16b610c6dac7f3453791cd696
kdmstj <rkddmstj429@gmail.com>
2023-07-28 09:55:31 +0900

    [fix] application build.gradle spring-cloud-starter dependency

--- src/main/java/com/duroop/dodom/DodomApplication.java
+++ src/main/java/com/duroop/dodom/DodomApplication.java
@@ -3,6 +3,7 @@ package com.duroop.dodom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableJpaAuditing
 @SpringBootApplication
